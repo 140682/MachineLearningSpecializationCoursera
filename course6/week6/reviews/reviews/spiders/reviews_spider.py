fce1c88164a738cd048761716e7719ef04003164
Andrey Starostin <astarfryazino@gmail.com>
2017-01-12 01:32:59 +0300

    scraper

--- course6/week6/reviews/reviews/spiders/reviews_spider.py
+++ course6/week6/reviews/reviews/spiders/reviews_spider.py
@@ -1,12 +1,21 @@
 import scrapy
 
-
+# to run:
+# scrapy crawl reviews -o reviews.json
 class QuotesSpider(scrapy.Spider):
     name = "reviews"
 
-    start_urls = ['https://market.yandex.ru/product/14206636/reviews']     
-
+    #start_urls = ['https://market.yandex.ru/product/14206636/reviews']     
+    start_urls = ['https://market.yandex.ru/catalog/54726/list?how=opinions&deliveryincluded=0&onstock=1']     
+    
     def parse(self, response):
+        # follow links to phone pages
+        for href in response.css('a.snippet-card__header-link::attr(href)').extract():
+            link = href[:href.index('?')] + '/reviews'
+            yield scrapy.Request(response.urljoin(link),
+                                 callback=self.parse_phone)
+
+    def parse_phone(self, response):
         for review in response.css('div.product-review-item'):            
             
             yield {
@@ -14,8 +23,8 @@ class QuotesSpider(scrapy.Spider):
                 'rating': review.css('div.rating::text').extract_first(),              
             }
             
-        #next_page = response.css('li.next a::attr(href)').extract_first()
+        # follow link to the next review page
         next_page = response.css('a.n-pager__button-next::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse_phone)
